6d8839673fd627f1ebbfa6188bc7a3298c6cc763
Konstantin <konstantin1020@gmail.com>
2021-12-07 13:48:35 -0800

    added JavaScript Arrays folder

---
+++ JavaScript Arrays/01_02/length_directassignment.js
@@ -0,0 +1,9 @@
+const pets = ['dog', 'cat', 'rabbit', 'hamster'];
+//Use the length property to determine how many objects are in the array
+
+//Using direct assignment, add 'snake' to the array in index position 2.
+const petsLength = pets.length;
+console.log(petsLength)
+
+pets[2] = "snake";
+console.log(pets)

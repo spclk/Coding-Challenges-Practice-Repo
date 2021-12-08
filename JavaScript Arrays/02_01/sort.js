a2698860f8485fb5d67bea795d221151d5ba01a7
Konstantin <konstantin1020@gmail.com>
2021-12-08 13:55:25 -0800

    updated

--- JavaScript Arrays/02_01/sort.js
+++ JavaScript Arrays/02_01/sort.js
@@ -1,11 +1,16 @@
 //Sort the following array alphabetically
-const pets = ['dog', 'cat', 'rabbit', 'hamster'];
-console.log(pets.sort())
+const pets = ["dog", "cat", "rabbit", "hamster"];
+console.log(pets.sort());
 
 const grades = [88, 94, 72, 99, 53, 77];
 //Sort the following array from highest to lowest
-grades.sort((a,b) => b - a)
-console.log(grades)
+grades.sort((a, b) => b - a);
+console.log(grades);
 //Sort the following array from lowest to highest
-grades.sort((a,b) => a - b)
-console.log(grades)
+grades.sort((a, b) => a - b);
+// or 
+const anotherWay = grades.sort((a, b) => {
+    return a - b;
+});
+
+console.log(grades);

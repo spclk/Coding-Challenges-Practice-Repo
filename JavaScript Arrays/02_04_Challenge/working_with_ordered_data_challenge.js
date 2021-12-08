945e5e16a1a6aa322aef3266d5117e1bc8b71b3d
Konstantin <konstantin1020@gmail.com>
2021-12-07 18:21:30 -0800

    solved 2-4

--- JavaScript Arrays/02_04_Challenge/working_with_ordered_data_challenge.js
+++ JavaScript Arrays/02_04_Challenge/working_with_ordered_data_challenge.js
@@ -9,9 +9,15 @@ const students = [
 //Challenge Question #1
 //Sort the array of students based on their grade,
 //in descending order(largest to smallest).
+students.sort((a, b) => b.grade - a.grade)
+console.log(students)
 
 //Challenge Question #2
 //After sorting the array, reverse the order of the array.
+students.reverse()
+console.log(students)
 
 //Challenge Question #3
 //Find a student in the array who has a grade over 90.
+const overNinety = students.find(element => element.grade > 90)
+console.log(overNinety)

8e68615917cfacf0cada0a6e1c12a7c0982e0416
Konstantin <konstantin1020@gmail.com>
2021-12-07 16:03:10 -0800

    solved 01_03

--- JavaScript Arrays/01_03/forEach.js
+++ JavaScript Arrays/01_03/forEach.js
@@ -4,3 +4,8 @@ const logScore = (score) => console.log('Score Over 65', score);
 
 // Iterate over the scores array using forEach.
 // Execute the logScores
+scores.forEach((element) =>  {
+    if (element > 65) {
+        logScore(element)
+    }
+})

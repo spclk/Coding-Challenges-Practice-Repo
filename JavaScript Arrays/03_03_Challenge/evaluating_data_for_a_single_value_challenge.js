bd4a6bcab4e769f2f4f61ce9e566d17b167ec48a
Konstantin <konstantin1020@gmail.com>
2021-12-07 19:23:20 -0800

    solved 3-3

--- JavaScript Arrays/03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
+++ JavaScript Arrays/03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
@@ -3,9 +3,15 @@ const bowlingScores = [154, 204, 300, 184, 286];
 
 //Challenge Question #1
 //Does the array of bowling scores include a 300?
+const includesThreeHundred = bowlingScores.includes(300)
+console.log(includesThreeHundred)
 
 //Challenge Question #2
 //Are some of the scores under 150?
+const underOneFifty = bowlingScores.some(element => element < 150)
+console.log(underOneFifty)
 
 //Challenge Question #3
 //Is every score an even number?
+const evenNumbers = bowlingScores.every(element => element % 2 === 0)
+console.log(evenNumbers)

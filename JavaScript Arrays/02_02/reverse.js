0dd91997968a021a740f9b28b3fe16b4dd7c359b
Konstantin <konstantin1020@gmail.com>
2021-12-07 17:29:10 -0800

    solved 2-2

--- JavaScript Arrays/02_02/reverse.js
+++ JavaScript Arrays/02_02/reverse.js
@@ -1,6 +1,14 @@
 const condiments = ['ketchup', 'mustard', 'relish', 'mayo'];
 // Use reverse to change the order of this array.
+console.log(condiments.reverse())
 
 const employees = ['Adams', 'Carter', 'Davis', 'Fraklin', 'Zappa'];
 // Use reverse to change the order of this array,
 // but keep the original array intact.
+
+// Use the spread operator to create a clone of the original array.
+const reversed = [...employees];
+
+reversed.reverse();
+console.log(reversed)
+console.log(employees)
